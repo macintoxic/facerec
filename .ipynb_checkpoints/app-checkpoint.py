8896b777f4896757450bd6d80c0e85337066c733
Charles Alves <charles.alves@nnumbers.com.br>
2021-04-19 16:52:24 -0300

    Initial commit

---
+++ .ipynb_checkpoints/app-checkpoint.py
@@ -0,0 +1,19 @@
+# simple_loopback.py
+from matplotlib import pyplot as plt
+import tensorflow as tf
+from mtcnn import MTCNN
+import numpy as np
+import cv2
+import streamlit as st
+from streamlit_webrtc import webrtc_streamer
+
+
+
+print("TF Version:", tf.__version__)
+print("CV2 Version:", cv2.__version__)
+
+st.header("WebRTC demo")
+webrtc_streamer(key="example")
+
+
+print(__name__)
