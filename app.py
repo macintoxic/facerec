d23b44c5d83f60d490cf1816122969355aa647f7
macintoxic <charles.alves@nnumbers.com.br>
2021-04-19 17:50:39 -0300

    remove cv2

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ from streamlit_webrtc import webrtc_streamer
 
 
 print("TF Version:", tf.__version__)
-print("CV2 Version:", cv2.__version__)
+#print("CV2 Version:", cv2.__version__)
 
 st.header("WebRTC demo")
 webrtc_streamer(key="example")
