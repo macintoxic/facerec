f4ba24bf113b6f155442a4e38c65d28838934dfe
macintoxic <charles.alves@nnumbers.com.br>
2021-04-19 19:50:41 -0300

    added stun server

--- app.py
+++ app.py
@@ -5,15 +5,23 @@ import tensorflow as tf
 import numpy as np
 #import cv2
 import streamlit as st
-from streamlit_webrtc import webrtc_streamer
-
-
+from streamlit_webrtc import webrtc_streamer, ClientSettings, VideoTransformerBase, WebRtcMode
 
 print("TF Version:", tf.__version__)
 #print("CV2 Version:", cv2.__version__)
 
-st.header("WebRTC demo")
-webrtc_streamer(key="example")
+
+WEBRTC_CLIENT_SETTINGS = ClientSettings(
+    rtc_configuration={"iceServers": [{"urls": ["stun:stun.l.google.com:19302"]}]},
+    media_stream_constraints={"video": True, "audio": False},
+)
+
+st.header("Face detect demo")
+rtc = webrtc_streamer(key="example", client_settings=WEBRTC_CLIENT_SETTINGS)
 
 
+dir(rtc)
 print(__name__)
+
+
+
